c4bb266a894ed0666456aaaace4cd6fd34174ce1
erpegypt <48647070+erpegypt@users.noreply.github.com>
2023-06-29 14:41:17 +0300

    Delete skyway/skyway/doctype/commission_payment directory

--- skyway/skyway/doctype/commission_payment/commission_payment.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, connect4systems.com and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Commission Payment', {
-	// refresh: function(frm) {
-
-	// }
-});
--- skyway/skyway/doctype/commission_payment/test_commission_payment.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, connect4systems.com and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestCommissionPayment(unittest.TestCase):
-	pass
